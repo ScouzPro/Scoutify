0bd13303e8f3c38c57899b96cc1f3e7ba242bafb
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-03-05 17:27:13 +0100

    create routes players,player, reports and basic navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayersComponent } from './pages/players/players.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { PlayerComponent } from './pages/player/player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {   path:'players',
+        component:PlayersComponent
+    },
+    {
+        path:'reports',
+        component:ReportsComponent
+    },
+    {   path:'player',
+        component:PlayerComponent
+    },
+    
+];
---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'] // Usa styleUrls en lugar de styleUrl
+})
+export class NavbarComponent {
+
+}
+
