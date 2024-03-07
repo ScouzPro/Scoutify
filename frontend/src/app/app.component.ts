a0cd96076e3a42e634a9a24a5c0d89c88cbe0af4
andreagarciacedeno <andreagarciacedeno@gmail.com>
2024-03-07 13:50:47 +0100

    create footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 
 @Component({
@@ -8,7 +9,7 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'server';
