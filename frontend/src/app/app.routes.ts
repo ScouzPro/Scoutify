4d9aa1fc6972e3b78514f5b94ee56beb0be988da
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-07 15:04:26 +0100

    hero component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,25 @@ import { Routes } from '@angular/router';
 import { PlayersComponent } from './pages/players/players.component';
 import { ReportsComponent } from './pages/reports/reports.component';
 import { LandingComponent } from './pages/landing/landing.component';
+// import { HeroLandingComponent } from './components/hero-landing/hero-landing.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { HeroLandingComponent } from './components/hero-landing/hero-landing.component';
 
 
 export const routes: Routes = [
     {
         path:'home',
-        component: LandingComponent
+        component: HeroLandingComponent
+    },
+    { 
+        path: 'login', 
+        component: AuthComponent 
+    },
+    { 
+        path: 'register', 
+        component: RegisterFormComponent
     },
     {   path:'players',
         component:PlayersComponent
@@ -16,6 +29,5 @@ export const routes: Routes = [
         path:'reports',
         component:ReportsComponent
     },
-   
     
 ];
--- frontend/src/app/components/hero-landing/hero-landing.component.ts
+++ frontend/src/app/components/hero-landing/hero-landing.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginFormComponent } from '../login-form/login-form.component';
+import { RegisterFormComponent } from '../register-form/register-form.component';
 
 @Component({
   selector: 'app-hero-landing',
   standalone: true,
-  imports: [],
+  imports: [LoginFormComponent, RegisterFormComponent],
   templateUrl: './hero-landing.component.html',
   styleUrl: './hero-landing.component.css'
 })
 export class HeroLandingComponent {
+  constructor(private router: Router) {} // Inject Router service
+
+  navigateToLogin() {
+    this.router.navigate(['/login']); 
+  }
+  navigateToRegister() {
+    this.router.navigate(['/register']); 
+  }
 
 }
