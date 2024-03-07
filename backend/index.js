0209e0fcc4d6e5decf53b51bf28c4106b6f759db
JeanCarlosPerez <jeancperez09@gmail.com>
2024-03-07 10:58:42 +0100

    add backend to players

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
 import { db } from "./database/database.js";
+import playerRouter from "./routes/playersRouter.js"
 
 dotenv.config();
 
 const app = express()
 app.use(express.json())
+app.use("/player", playerRouter)
+
+db()
 
 app.listen(3001, console.log("Conneted"))
-db()
