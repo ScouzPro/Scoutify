a55a466f68b63bbc87dd0f3c3c8b35b525fc14ff
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-08 09:28:52 +0100

    added component

---
+++ frontend/src/app/pages/auth-reg/auth-reg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-reg',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-reg.component.html',
+  styleUrl: './auth-reg.component.css'
+})
+export class AuthRegComponent {
+
+}
