353d392520909348c032c1621ce94bd1bf99b1de
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-13 15:28:10 +0100

    autentificacion reg/login correctas y completas

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { NgIf } from '@angular/common';
 
@@ -15,13 +16,34 @@ import { NgIf } from '@angular/common';
 })
 export class AuthComponent {
     //VALIDACIONES
-    //Las reglas de las validaciones van aqui, en este export vamos a colocar los campos que queremos que tengan validaciones, y que van a funcionar a modo de estados,
-    //con las propiedades de Angular.
+    //Las reglas de las validaciones van aqui, en este export vamos a colocar los campos que queremos que tengan validaciones, y que van a funcionar a modo de estados, con las propiedades de Angular.
+
+    //GETTERS
+    get name(){
+        return this.formUser.get('name') as FormControl; //acceso al grupo, luego a la propiedad get y luego al valor
+    }
+    get email(){
+        return this.formUser.get('email') as FormControl; //acceso al grupo, luego a la propiedad get y luego al valor
+    }
+    get password(){
+        return this.formUser.get('password') as FormControl; //acceso al grupo, luego a la propiedad get y luego al valor
+    }
+    
+    //Para asegurarnos que todas las validaciones paran en el formulario antes de enviar:Ç
+    //GRUPO DE CONTROLADORES 
+    formUser = new FormGroup({ //esto a la cabeda del form
+        'name': new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]/)]),
+        'email': new FormControl('', [Validators.required, Validators.email]),
+        'password': new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]),
+    })
+
+
     //A modo de estados, llamamos a los inputs que tenemos en el html, que tinen una propiedad de FormControl, y les pasamos la funcionalidad validators, indicando que son requeridos (las '' vacias para que no muestre nada dentro de los campos)
-    name = new FormControl('', Validators.required);
+    //CONTROLADORES DE FORMULARIO
+    // name = new FormControl('', Validators.required);
     //Cuando tengamos más de una forma de validación, deben ser en un array
-    email = new FormControl('', [Validators.required, Validators.email]);
-    password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+    // email = new FormControl('', [Validators.required, Validators.email]);
+    // password = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
 
 
