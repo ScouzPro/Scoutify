54c308fe3dea349fe1d235f83d5d70a3503a4188
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-13 12:02:23 +0100

    autentificacion de login correcta

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
@@ -6,15 +6,14 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { HeroLandingComponent } from './components/hero-landing/hero-landing.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
-// import { PlayerMetricsComponent } from './components/pruebaMetrics/pruebaMetrics-component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent , FooterComponent, LandingComponent, HeroLandingComponent, LoginFormComponent, RegisterFormComponent]
+    imports: [RouterOutlet, ReactiveFormsModule, NavbarComponent , FooterComponent, LandingComponent, HeroLandingComponent, LoginFormComponent, RegisterFormComponent]
 })
 export class AppComponent {
   title = 'Scoutify';
--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-auth',
     standalone: true,
     templateUrl: './auth.component.html',
     styleUrl: './auth.component.css',
-    imports: [FooterComponent]
+    imports: [NgIf, ReactiveFormsModule, FooterComponent]
+    //NgIf es una directiva de condicionales propia de Angular, que evalua el estado de algo y lo utiliza en base a las instrucciones dadas
 })
 export class AuthComponent {
+    //VALIDACIONES
+    //Las reglas de las validaciones van aqui, en este export vamos a colocar los campos que queremos que tengan validaciones, y que van a funcionar a modo de estados,
+    //con las propiedades de Angular.
+    //A modo de estados, llamamos a los inputs que tenemos en el html, que tinen una propiedad de FormControl, y les pasamos la funcionalidad validators, indicando que son requeridos (las '' vacias para que no muestre nada dentro de los campos)
+    name = new FormControl('', Validators.required);
+    //Cuando tengamos más de una forma de validación, deben ser en un array
+    email = new FormControl('', [Validators.required, Validators.email]);
+    password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+
+
+
+    //Constructor para las rutas de navegación de la pagina
     constructor (private router: Router) {}
     navigateToHeroLanding() { //Ruta que vueve a landing al pulsar btn
         this.router.navigate([""]);
