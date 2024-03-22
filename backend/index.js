490a5eb2b003553664300b582f165faa1f76bb77
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-03-22 11:42:58 +0100

    updates render

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"; 
+import cors from "cors";
 import { db } from "./database/database.js";
-import playerRouter from "./routes/playersRouter.js"
+import playerRouter from "./routes/playersRouter.js";
 import reportRoutes from "./routes/reportRoutes.js";
-import playerMetricsRouter from './routes/metricRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import seasonRoutes from './routes/seasonRoutes.js'
+import playerMetricsRouter from "./routes/metricRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import seasonRoutes from "./routes/seasonRoutes.js";
+import path from "path";
 
 dotenv.config();
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+})
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 
 //RUTAS DE USO
 app.use(cors());
-app.use('/', playerMetricsRouter);
-app.use('/api', reportRoutes);
-app.use("/player", playerRouter)
-app.use("/season", seasonRoutes)
+app.use("/", playerMetricsRouter);
+app.use("/api", reportRoutes);
+app.use("/player", playerRouter);
+app.use("/season", seasonRoutes);
 app.use("/auth", authRoutes);
 
 //PUERTO Y DB
 db();
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
