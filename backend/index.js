59cf3e563fbda128a8fcec30b32c5a9297dd5151
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-22 10:59:37 +0100

    render testing

--- backend/index.js
+++ backend/index.js
@@ -31,18 +31,9 @@ app.listen(PORT, () => {
 
 //RENDER
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname,
-
-'/frontend/dist')));
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
 
 app.get('*' , (req, res) => {
 
-res.sendFile(path.join(__dirname,
-
-'frontend'
-,
-'dist'
-,
-
-'index.html'));
+res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
 })
