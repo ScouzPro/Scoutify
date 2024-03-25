34981c6fe79d8f42804f1053220fe09dd5ce8095
Almudena Rivera <riveraferalmudena@gmail.com>
2024-03-25 21:25:16 +0100

    updated styles in components

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,8 +10,7 @@ module.exports = {
     "custom-yellow": "#F2D16D",
     "custom-red": "#C84343",
     "custom-white": "#F2F2F2",
-    "custom-black": "#1E1E1E",
-    "custom-gray": "#A19DA8",
+    "custom-black": "#1E1E1E"
   },
   backgroundImage: {
     "gradient-yellow": "linear-gradient(to bottom right, white, #F2D16D)", // Define el gradiente personalizado
